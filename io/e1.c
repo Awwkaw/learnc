e9eb5f56305b2a4d0b1d992da923de14d62b1e50
tekc <awwkaw@gmail.com>
2018-10-17 16:19:59 +0200

    initial commit

---
+++ io/e1.c
@@ -0,0 +1,8 @@
+#include <stdio.h> //standerd input output library, included printf() and scanf()
+
+int main()	//This function must exist for the script to be valid.
+{
+	printf("Hello world"); //Prints  the sring
+	return 0; //The function type was int, so an interger must be returned
+}
+
