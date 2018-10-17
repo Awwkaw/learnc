7cebc8bcd968979b03a95a982ff6773566c737c2
tekc <awwkaw@gmail.com>
2018-10-17 16:22:35 +0200

    the second example

---
+++ io/e2.c
@@ -0,0 +1,9 @@
+#include <stdio.h> 
+//Thius file has interger output
+
+int main()
+{
+	int testInteger = 5;
+	printf("The number is %d", testInteger); //note the digit is an integer
+	return 0;
+}
