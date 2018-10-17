7682089b87654374360be4be167816bb623771dd
tekc <awwkaw@gmail.com>
2018-10-17 16:56:28 +0200

    the sixth example on the sites io section

---
+++ io/e6.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+//This time weel just print the character with the ASCII alue 69
+
+int main()
+{
+	int chr=69;
+	printf("The character stored in the ASCII table spot %d is %c ",chr, chr);
+	return 0;
+}
